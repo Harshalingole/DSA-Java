e464746fe9aa209b39e163f2497150fe51d2532f
Harshalingole <harshal.ingole21@gmail.com>
2022-11-24 07:03:59 +0530

    insertion sort

---
+++ Kunal_Kushwah/SortingAlgo/BasicSortAlgo/InsertionSort.java
@@ -0,0 +1,17 @@
+package Kunal_Kushwah.SortingAlgo.BasicSortAlgo;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        // int[] arr = {3,6,2,1,8,7,4,5,3,1};
+        int[] arr = {5,1,3,4,2};
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr){
+        // pick an element form unsorted part and placed at the right position in sorted part
+    }
+}
