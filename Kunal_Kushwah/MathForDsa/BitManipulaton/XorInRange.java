755d536d519fb922d6b56d952501e79ebafafe08
Harshalingole <harshal.ingole21@gmail.com>
2022-12-05 05:59:25 +0530

    Bit - find XOR of no from 0 to a

---
+++ Kunal_Kushwah/MathForDsa/BitManipulaton/XorInRange.java
@@ -0,0 +1,17 @@
+package Kunal_Kushwah.MathForDsa.BitManipulaton;
+
+public class XorInRange {
+    public static void main(String[] args) {
+        int range = 8;
+        
+        // find XOR of no from 0 to a
+        /*
+        There is pattern in this question after 4th Xor its repeat itself
+        ----Observation---
+        a % 4 = 0; => ans = a 
+        a % 4 = 1; => ans = 1
+        a % 2 = 2; => ans = a + 1
+        a % 4 = 3 ==> ans = 0
+        */
+    }
+}
