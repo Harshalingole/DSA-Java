4a0fe3af69f370e1d91f74423aefec664498dcbc
Harshalingole <harshal.ingole21@gmail.com>
2022-12-09 19:01:11 +0530

    Bubble Sort -modified

--- Kunal_Kushwah/SortingAlgo/BasicSortAlgo/BubbleSort.java
+++ Kunal_Kushwah/SortingAlgo/BasicSortAlgo/BubbleSort.java
@@ -2,8 +2,8 @@ package Kunal_Kushwah.SortingAlgo.BasicSortAlgo;
 
 public class BubbleSort {
     public static void main(String[] args){
-        // int[] arr = {3,6,2,1,8,7,4,5,3,1};
-        int[] arr = {5,1,3,4,2};
+        int[] arr = {3,6,2,1,8,7,4,5,3,1};
+        // int[] arr = {5,1,3,4,2};
         bubbleSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
